fa483cb85c9b8194c48eb2ec7eeceb756bc15b3d
KayenM <kayenkmehta@gmail.com>
2023-08-31 22:08:26 -0400

    Change redirect url

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import {useRouter } from 'next/router';
 
 const CLIENT_ID = "00c9d3d652c94e69984fdd134ed41c18";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/home";
+const REDIRECT_URL_AFTER_LOGIN = "https://statify-chi.vercel.app/home";       //For local testing: http://localhost:3000/home
 const SPACE_DELIMITER = "%20";
 const SCOPES = [
   "user-read-currently-playing",
